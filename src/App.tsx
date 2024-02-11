15eca168e509f0eb5e8261585afab28600713f4e
Sergey_js15 <serj.777.front@gmail.com>
2024-02-11 11:24:24 +0200

    add two brange

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { FC } from "react"
+import { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchProduct } from "./store/productSlice";
 
 const App: FC = () => {
- 
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProduct())
+  },[])
+
   return (
    <main>
        <h1>Home</h1>
---
+++ src/store/productSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+
+interface IProduct {
+    id: number;
+    name: string;
+    title: string
+};
+
+interface IProductState {
+    data: IProduct[],
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string | null
+};
+
+export const fetchProduct = createAsyncThunk<IProduct[]>(
+    'data/fetchData',
+    async () => {
+        const response = await fetch('https://dummyjson.com/products/');
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error('Error data fetch');
+        }
+        return data.products as IProduct[]
+    }
+);
+
+const initialState: IProductState = {
+    data: [],
+    status: 'idle',
+    error: null 
+}
+
+const productSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProduct.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchProduct.fulfilled, (state, action: PayloadAction<IProduct[]>) => {
+                state.status = 'succeeded';
+                state.data = action.payload
+                state.error = null;
+            })
+            .addCase(fetchProduct.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.payload.message
+                state.data = [];
+            })
+    },
+})
+
+export default productSlice.reducer;
