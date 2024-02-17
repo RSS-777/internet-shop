106d498b86b5fbefcc677be089c6fb17de39cf92
Sergey_js15 <serj.777.front@gmail.com>
2024-02-17 15:51:39 +0200

    add style DetailsProduct

--- src/components/DetailsProduct/DetailsProduct.tsx
+++ src/components/DetailsProduct/DetailsProduct.tsx
@@ -1,18 +1,32 @@
 import { FC } from "react";
 import { TProduct } from "../../store/productSlice";
-import { StyelButton, StyelContainerDetails } from "./StyleDetailsProduct";
+import { StyelButton, StyelContainerDetails, StyleBlockImages, StyleFlex, StyleTextBlock } from "./StyleDetailsProduct";
 
 interface IDetailsProduct {
     dataProps: TProduct | null;
     onClick: () => void;
 }
 
-export const DetailsProduct: FC<IDetailsProduct> = ({dataProps, onClick}) => {
-    return(
+export const DetailsProduct: FC<IDetailsProduct> = ({ dataProps, onClick }) => {
+    return (
         <StyelContainerDetails>
-           <h3>{dataProps?.brand}</h3>
-           <div>{dataProps?.images.map((item, i) => <img key={i} src={item} alt="Image product"/>)}</div> 
-           <StyelButton onClick={onClick}>&#8592; Back</StyelButton>
+            <h2>{dataProps?.brand}</h2>
+            <StyleTextBlock>
+                <p>Title: <span>{dataProps?.title}</span></p>
+                <p>Price: <span>{dataProps?.price}$</span></p>
+                <p>Discount: <span>{dataProps?.discountPercentage}$</span></p>
+                <p>Rating: <span>{dataProps?.rating}</span></p>
+                <p>Stock: <span>{dataProps?.stock}</span></p>
+                <p>Description: <span>{dataProps?.description}</span></p>
+            </StyleTextBlock>
+            <StyleFlex>
+                {dataProps?.images.map((item, i) =>
+                    <StyleBlockImages>
+                        <img key={i} src={item} alt="Image product" />
+                    </StyleBlockImages>
+                )}
+            </StyleFlex>
+            <StyelButton onClick={onClick}>&#8592; Back</StyelButton>
         </StyelContainerDetails>
     )
 }
--- src/components/DetailsProduct/StyleDetailsProduct.ts
+++ src/components/DetailsProduct/StyleDetailsProduct.ts
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 export const StyelContainerDetails = styled.div`
    position: relative;
+
+   h2 {
+    text-align: center;
+    margin: 20px;
+    color: red;
+   }
 `;
 
 export const StyelButton = styled.button`
@@ -20,4 +26,47 @@ export const StyelButton = styled.button`
     box-shadow: 1px 1px 3px 0 black;
     border: 1px solid tomato;
   }
+`;
+
+export const StyleFlex = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-top: 20px; 
+`;
+
+export const StyleBlockImages = styled.div`
+  display: flex;
+  max-width: 800px;
+  box-shadow: 2px 2px 12px 1px black;
+  border-radius: 20px;
+  overflow: hidden;
+  
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
+`;
+
+export const StyleH2 = styled.h2`
+  text-align: center;
+  margin: 20px;
+  color: red;
+`;
+
+export const StyleTextBlock = styled.div`
+   width:fit-content;
+
+   p {
+       color: blue;
+       font-weight: bold;
+       text-shadow: 0 0 20px black;
+    };
+
+    span {
+     color: #A12C00;
+     margin-left: 5px;
+     font-family: cursive;
+    };
 `;
