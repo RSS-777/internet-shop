c28d9bc2369f4a624818369ba96f45ca0a21eb2c
Sergey_js15 <serj.777.front@gmail.com>
2024-03-02 19:10:57 +0200

    adaptive footer

--- src/components/Footer/FooterStyle.ts
+++ src/components/Footer/FooterStyle.ts
@@ -29,9 +29,14 @@ export const NavLinkStyle = styled(NavLink)`
 export const FooterStyle = styled.footer`
   background-color: #90A2F1;
   
-  div {
-    display: flex;
+  >div {
+    display: grid;
+    grid-template: 1fr / 1fr 1fr;
     justify-content: space-between;
+
+    @media (max-width: 580px) {
+      grid-template: 1fr 1fr / 1fr;
+    }
   }
   
   >div:nth-child(2) {
@@ -62,8 +67,11 @@ export const NavStyle = styled.nav`
 `;
 
 export const BlockSocialStyle = styled.div`
+  display: flex;
+  justify-content: flex-end;
   height: 24px;
   margin: 10px 20px;
+  min-width: 320px;
 
   p {
     color: blue;
@@ -84,6 +92,35 @@ export const BlockSocialStyle = styled.div`
           border-radius: 50%;
         }
       }
+
+      @media (max-width: 776px) {
+        margin-left: 25px;
+      }
+
+      @media (max-width: 580px) {
+        margin-left: 10px;
+      }
+
+      @media (max-width: 380px) {
+        margin-left: 8px;
+      }
     }
+   }
+
+   @media (max-width: 776px){
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-end;
+    height: 64px;
+    min-width: 220px;
+   }
+
+   @media (max-width: 580px){
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    line-height: 10px;
+    height: 24px;
+    margin: 10px 10px;
    }
 `;
